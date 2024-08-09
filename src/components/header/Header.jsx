a41e0b1fdf096b36024fd3c78a5e8e12e5fa0f7e
Matias Martearena <mmartearena@asap-consulting.net>
2024-08-09 10:32:22 -0300

    add: navbar styles and responsive

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -11,16 +11,16 @@ const Header = () => {
 
     return (
         <header>
-            <nav className="bg-white border-gray-200 dark:bg-gray-900 dark:border-gray-700">
+            <nav className="bg-[#282121]">
                 <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
                     <a href="/" className="flex items-center space-x-3 rtl:space-x-reverse">
                         <img src={logo} className="h-8" alt="Flowbite Logo" />
-                        <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">Travel MA</span>
+                        <span className="self-center text-2xl font-semibold whitespace-nowrap text-[#eae5d9]">Travel MA</span>
                     </a>
                     <button
                         onClick={toggleMenu}
                         type="button"
-                        className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
+                        className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-[#eae5d9] rounded-lg md:hidden hover:outline-none hover:ring-2 hover:ring-gray-200"
                         aria-controls="navbar-dropdown"
                         aria-expanded={isOpen}>
                         <span className="sr-only">Open main menu</span>
@@ -29,11 +29,11 @@ const Header = () => {
                         </svg>
                     </button>
                     <div className={`${isOpen ? 'block' : 'hidden'} w-full md:block md:w-auto`} id="navbar-dropdown">
-                        <ul className="flex flex-col font-medium p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
+                        <ul className="flex flex-col font-medium p-4 md:p-0 mt-4 rounded-lg border-[#282121] bg-[#eae5d9] md:bg-[#282121] md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0">
                             <li>
                                 <a
                                     href="/"
-                                    className="block py-2 px-3 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 md:dark:text-blue-500 dark:bg-blue-600 md:dark:bg-transparent"
+                                    className="block py-2 px-3 text-[#282121] font-semibold rounded md:bg-transparent md:text-[#eae5d9] md:p-0 md:dark:bg-transparent hover:underline"
                                     aria-current="page">
                                     Home
                                 </a>
@@ -41,7 +41,7 @@ const Header = () => {
                             <li>
                                 <a
                                     href="/city-list"
-                                    className="block py-2 px-3 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 md:dark:text-blue-500 dark:bg-blue-600 md:dark:bg-transparent"
+                                    className="block py-2 px-3 text-[#282121] font-semibold rounded md:bg-transparent md:text-[#eae5d9] md:p-0 md:dark:bg-transparent hover:underline"
                                     aria-current="page">
                                     List of cities
                                 </a>
--- src/components/main/Main.jsx
+++ src/components/main/Main.jsx
@@ -6,8 +6,15 @@ import Team from '../team/Team'
 const Main = ({ team }) => {
     return (
         <main>
-            <About />
-            <Team team={team} />
+            <div>
+                <About />
+                <Team team={team} />
+            </div>
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320">
+                <path
+                    fill="#ebd5b8"
+                    d="M0,128L48,117.3C96,107,192,85,288,90.7C384,96,480,128,576,122.7C672,117,768,75,864,53.3C960,32,1056,32,1152,58.7C1248,85,1344,139,1392,165.3L1440,192L1440,0L1392,0C1344,0,1248,0,1152,0C1056,0,960,0,864,0C768,0,672,0,576,0C480,0,384,0,288,0C192,0,96,0,48,0L0,0Z"></path>
+            </svg>
         </main>
     )
 }
